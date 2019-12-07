4db5f7eb35d515e271e15613bc909ba8de5fe113
Yikunoamlak Abraham <yikuno@Yikunoamlaks-MacBook-Pro.local>
2019-12-07 15:04:26 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './compenents/Layouts/Layouts'
+
+function App() {
+  return (
+    <div >
+     <Layout>
+      <p>this is app</p>
+     </Layout>
+    </div>
+  );
+}
+
+export default App;
