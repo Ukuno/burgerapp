91da71ad1c74276aa6dc0eb7bb4fad9a38fe2673
Yikunoamlak Abraham <yikuno@Yikunoamlaks-MacBook-Pro.local>
2020-01-20 17:27:01 -0600

    add layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layouts/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
+      <div>
+       <Layout>
+         <p>This is layout</p>
+       </Layout>
       </div>
     );
   }
