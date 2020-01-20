4f476450e60ffbe135b45a3166397f436a51b388
Yikunoamlak Abraham <yikuno@Yikunoamlaks-MacBook-Pro.local>
2020-01-20 16:38:44 -0600

    css module config change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Layout from './compenents/Layouts/Layouts'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  return (
-    <div >
-     <Layout>
-      <p>this is app</p>
-     </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
