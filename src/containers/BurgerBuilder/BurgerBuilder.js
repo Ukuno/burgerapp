4075608d0c190483d6605a92244e9b286a34cd1d
Yikunoamlak Abraham <yikuno@Yikunoamlaks-MacBook-Pro.local>
2020-10-18 12:12:35 -0500

    add firebase post

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/Ui/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES =    {
     cheese : 0.5,
@@ -76,7 +77,26 @@ class BurgerBuilder extends Component {
     }
 
     continuePurchsing = () => {
-        alert('you buy it')
+        const order = {
+            ingredients : this.state.ingredients,
+            price: this.state.totalPrice.toFixed(2),
+            customer : {
+                name: 'Fili',
+                email: 'fili@fili.com',
+                address: {
+                    street: '1234st streetName',
+                    zipcode: '12345',
+                    country : 'USA'
+                }
+            },
+            deliverMethod: 'pickup'
+        }
+        axios.post('/order.json', order)
+        .then(res => {
+            console.log(res)
+            this.closeModal();
+        })
+        .catch(err => console.log(err));
     }
 
 
